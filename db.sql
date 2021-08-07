39c41652fd11669dbe165c9f6bc11edbd6ae7212
Hassan Tahir <info.hassantahir@gmail.com>
2021-08-07 18:49:18 +0500

    SQL db

---
+++ db.sql
@@ -0,0 +1,11 @@
+-- ### CREATING DATABASE
+-- ### AUTHOR: HASSAN TAHIR
+
+CREATE TABLE products (
+
+    id INT,                --numbers
+    name VARCHAR(60),      --characters
+    price INT,             --numbers
+    on_sale boolean,       --decisions
+
+);
