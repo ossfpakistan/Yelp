ddabff2dbc38e8216f87aae4f5973aad809292c2
Hassan Tahir <hassan.tahir.mcc@gmail.com>
2021-08-08 23:51:34 +0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -9,3 +9,18 @@ CREATE TABLE products (
     on_sale boolean,       --decisions
 
 );
+
+
+ALTER TABLE products ADD COLUMN featured boolean;
+ALTER TABLE products DROP COLUMN featured;
+
+
+CREATE TABLE restaurants (
+    id INT,
+    name VARCHAR(50),
+    location VARCHAR(50),
+    price_range INT
+
+);
+
+INSERT INTO restarants (id, name, location, price_range) values (123, 'mcdonalds', 'new yorks', '3');
