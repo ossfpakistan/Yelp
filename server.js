2a786f2d6e1c9154e8b46b76d1718be09367dc2f
Hassan Tahir <hassan.tahir.mcc@gmail.com>
2021-08-29 12:12:31 +0500

    Middleware & new || for creating restaurants

--- server.js
+++ server.js
@@ -6,16 +6,34 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-
+ 
 //Setting up middlewares 
 const app = express();
+app.use((req, res, next) => {
+    console.log("placement of second middleware");
+    next();
+});
 
+app.use((req, res, next) => {
+    console.log("main middleware");
+    next();
+});
 
 //making port for the live
 const port = 3005;
 
 app.listen(port, () => {
     console.log(`server is online and listening on port ${port}`);
+});
+
+//Creating restaurants
+app.get("/api/v1/restaurants/:restaurant:id", (req, res) => {
+    console.log(req.body);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`server is up and listening on port ${port}`);
 });
 
 //setting up API for fetching data
