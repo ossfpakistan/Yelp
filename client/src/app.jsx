53e07848afb1f09e4a51b8ef0114a77ad4751b1e
Hassan Tahir <hassan.tahir.mcc@gmail.com>
2021-09-11 17:05:29 +0500

    Update app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,18 @@
 //Making react components
 
 //Importing libs
-import React from'react'
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 const App = () => {
-    return <div>App</div>
-}
+    return <div>
+        <Router>
+            <Route exact path="/" component={home}/>
+            <Route exact path="/resturants/:id/update" component={updatepage}/>
+            <Route exact path="/updatepage" component={home}/>
+        </Router>
+    </div>;
+};
 
 //Exporting components
 export default App;
